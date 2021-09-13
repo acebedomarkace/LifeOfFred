0cd73431b3d25d844696f4df46d432b5bb4ef554
ghostwriter <acebedo.mark@gmail.com>
2021-09-13 11:57:19 +0800

    Added Apples Chapter 2 Game

---
+++ Apples/Apples_Chapter02.jl
@@ -0,0 +1,42 @@
+### A Pluto.jl notebook ###
+# v0.16.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ fc1a33b0-1444-11ec-11a8-e5136f52fe8b
+md"`Student Name:` <Enter Name Here>  
+
+`Lesson Date:` <Enter Date Here>"
+
+# ╔═╡ 039c6f05-fd79-4e61-8ba6-43c7c08650a9
+md"# Chapter 2: Closer to Sunrise  
+### Your Turn to Play"
+
+# ╔═╡ 9e0140ec-297c-4af1-8884-f6f2e9af2eed
+md"1. Fred made six drawings. Fred's six drawings plus Kingie's one drawing equals how many drawings? (Type your answer in between the quotes of the `md\"\"` below then press the play button)"
+
+# ╔═╡ 72e6aff1-1712-4ef9-a5ad-8b914597a1c2
+md""
+
+# ╔═╡ e49ed17f-8985-4c9f-9c24-eda083d7e7b2
+md"2. Fill in the blank: 5 + `_` = 7"
+
+# ╔═╡ d61d4a96-b223-4ec6-82a1-3ba0a8df34a3
+
+
+# ╔═╡ 13c8029f-80da-4513-a4bb-8ec64b87d355
+md"3. Fill in the blank: 4 + `_` = 7"
+
+# ╔═╡ 70d28920-e978-48d6-a97e-52423e012bcc
+
+
+# ╔═╡ Cell order:
+# ╟─fc1a33b0-1444-11ec-11a8-e5136f52fe8b
+# ╟─039c6f05-fd79-4e61-8ba6-43c7c08650a9
+# ╟─9e0140ec-297c-4af1-8884-f6f2e9af2eed
+# ╠═72e6aff1-1712-4ef9-a5ad-8b914597a1c2
+# ╟─e49ed17f-8985-4c9f-9c24-eda083d7e7b2
+# ╠═d61d4a96-b223-4ec6-82a1-3ba0a8df34a3
+# ╟─13c8029f-80da-4513-a4bb-8ec64b87d355
+# ╠═70d28920-e978-48d6-a97e-52423e012bcc
