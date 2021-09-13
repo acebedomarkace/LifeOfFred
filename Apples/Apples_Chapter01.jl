64fe55585d197d29e635d96aa3a3f45577d34f3c
ghostwriter <acebedo.mark@gmail.com>
2021-09-13 11:39:20 +0800

    First Commit

---
+++ Apples/Apples_Chapter01.jl
@@ -0,0 +1,43 @@
+### A Pluto.jl notebook ###
+# v0.16.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ be2d1781-1b34-4cb6-89a2-0a1c36f94f66
+md"`Student Name:` <Enter Name Here> 
+
+`Lesson Date:` i.e. 12-September-2021 (Sunday)"
+
+# ╔═╡ 47c33864-139d-11ec-17fc-f1b81c6cd635
+md"# Chapter 1: Early in the Morning  
+### Your Turn to Play"
+
+# ╔═╡ d914b817-d48e-41bf-87ce-86ebd472ec49
+md"1. Sometimes, we write \"6 + 1 is 7\". Sometimes, we use an equals sign and write \"6 + 1 = 7.\"  How would you write \"4 + 3 is 7\" using an equals sign? (Type your answer in between the quotes of `md\"<your answer here>\"` below)."  
+
+
+# ╔═╡ 70d35eee-ff78-4c52-a537-ac5e95248a3b
+md"4+3=7"
+
+# ╔═╡ 588979e1-ac63-4436-90cf-d1a6ec446779
+md"2. We know that 4 pencils plus 3 pencils equals 7 pencils. What does 4 trees plus 3 trees equal?"
+
+# ╔═╡ 38134368-f237-43e7-9b8a-3b2d43f96140
+4+3
+
+# ╔═╡ 88891864-771c-4b88-a0f2-28fdf481dfea
+md"3. We know that 5 + 2 = 7.  What does 2 + 5 equal?"
+
+# ╔═╡ 7faac205-ab01-4a30-ae0d-9e811318ae85
+2+5
+
+# ╔═╡ Cell order:
+# ╟─be2d1781-1b34-4cb6-89a2-0a1c36f94f66
+# ╟─47c33864-139d-11ec-17fc-f1b81c6cd635
+# ╟─d914b817-d48e-41bf-87ce-86ebd472ec49
+# ╟─70d35eee-ff78-4c52-a537-ac5e95248a3b
+# ╟─588979e1-ac63-4436-90cf-d1a6ec446779
+# ╠═38134368-f237-43e7-9b8a-3b2d43f96140
+# ╟─88891864-771c-4b88-a0f2-28fdf481dfea
+# ╠═7faac205-ab01-4a30-ae0d-9e811318ae85
