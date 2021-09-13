1145ea7706a07cfabd4aeabb4f3223b4e1607069
ghostwriter <acebedo.mark@gmail.com>
2021-09-13 12:09:23 +0800

    Added Chapter 3 Exercises

---
+++ Apples/Apples_Chapter03.jl
@@ -0,0 +1,62 @@
+### A Pluto.jl notebook ###
+# v0.16.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ fc1a33b0-1444-11ec-11a8-e5136f52fe8b
+md"`Student Name:` <Enter Name Here>  
+
+`Lesson Date:` <Enter Date Here>"
+
+# ╔═╡ 039c6f05-fd79-4e61-8ba6-43c7c08650a9
+md"# Chapter 3: What to Do before Dawn  
+### Your Turn to Play"
+
+# ╔═╡ 9e0140ec-297c-4af1-8884-f6f2e9af2eed
+md"1. February is the most misspelled month. Guess which is the most misspelled day of the week: Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, or Saturday. (Type your answer in between the quotes of the `md\"\"` below then press the play button)"
+
+# ╔═╡ 72e6aff1-1712-4ef9-a5ad-8b914597a1c2
+md""
+
+# ╔═╡ e49ed17f-8985-4c9f-9c24-eda083d7e7b2
+md"2. Sunday is the *first* day of the week. Monday is the *second* day of the week. What day of the week is Saturday?"
+
+# ╔═╡ d61d4a96-b223-4ec6-82a1-3ba0a8df34a3
+md""
+
+# ╔═╡ 13c8029f-80da-4513-a4bb-8ec64b87d355
+md"3.  4 + ? = 7"
+
+# ╔═╡ 70d28920-e978-48d6-a97e-52423e012bcc
+
+
+# ╔═╡ 8192a732-8831-4eb8-af23-f43abc78f55f
+md"4. 5 + ? = 7"
+
+# ╔═╡ 94533f70-d100-4e2b-9a29-f3f70997fa85
+
+
+# ╔═╡ e407b9a6-60a5-49b5-95b2-b0b2d4979627
+md"5. 3 + ? = 7"
+
+# ╔═╡ 53ce5144-bc04-4941-a5bc-6a806d9db80e
+md"6. Which animal did Fred draw?"
+
+# ╔═╡ 895d5adc-f161-4f9c-a2d1-353d6f223ad5
+md""
+
+# ╔═╡ Cell order:
+# ╟─fc1a33b0-1444-11ec-11a8-e5136f52fe8b
+# ╟─039c6f05-fd79-4e61-8ba6-43c7c08650a9
+# ╟─9e0140ec-297c-4af1-8884-f6f2e9af2eed
+# ╠═72e6aff1-1712-4ef9-a5ad-8b914597a1c2
+# ╟─e49ed17f-8985-4c9f-9c24-eda083d7e7b2
+# ╠═d61d4a96-b223-4ec6-82a1-3ba0a8df34a3
+# ╟─13c8029f-80da-4513-a4bb-8ec64b87d355
+# ╠═70d28920-e978-48d6-a97e-52423e012bcc
+# ╟─8192a732-8831-4eb8-af23-f43abc78f55f
+# ╠═94533f70-d100-4e2b-9a29-f3f70997fa85
+# ╟─e407b9a6-60a5-49b5-95b2-b0b2d4979627
+# ╟─53ce5144-bc04-4941-a5bc-6a806d9db80e
+# ╠═895d5adc-f161-4f9c-a2d1-353d6f223ad5
